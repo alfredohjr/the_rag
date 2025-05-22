66b58959773d6f46f355ea71e4f261e2d30bcfc1
alfredohjr <alfredojrgasper@gmail.com>
2025-05-22 18:12:24 -0300

    ajustando função

--- src/FixVectorFiles.py
+++ src/FixVectorFiles.py
@@ -1,37 +1,44 @@
 import json
 import os
 
-path = 'tmp/vector_files'
-for file in os.listdir(path):
+def run():
+    path = 'tmp/vector_files'
+    for file in os.listdir(path):
 
-    if not file.endswith('.json'):
-        continue
-
-    print(f'Inicio : {file}')
-    
-    data = {}
-    with open(f'{path}/{file}','r',encoding='latin1') as f:
-        data = json.load(f)
-    
-    tamanho_original = len(data)
-    print(f'Itens : {tamanho_original}')
-    
-    new_data = []
-    index = []
-    for d in data:
-        if d['text'] in index:
+        if not file.endswith('.json'):
             continue
-        new_data.append(d)
-        index.append(d['text'])
-    
-    tamanho_apos_verificacao = len(new_data)
-    print(f'Tamanho apos checar : {tamanho_apos_verificacao}')
 
-    if tamanho_original == tamanho_apos_verificacao:
-        continue
-    
-    with open(f'{path}/{file}', "w", encoding="utf-8") as f:
-        json.dump(new_data, f, ensure_ascii=False, indent=2)
+        print(f'Inicio : {file}')
+        
+        data = {}
+        with open(f'{path}/{file}','r',encoding='latin1') as f:
+            data = json.load(f)
+        
+        tamanho_original = len(data)
+        print(f'Itens : {tamanho_original}')
+        
+        new_data = []
+        index = []
+        for d in data:
+            if d['text'] in index:
+                continue
+            new_data.append(d)
+            index.append(d['text'])
+        
+        tamanho_apos_verificacao = len(new_data)
+        print(f'Tamanho apos checar : {tamanho_apos_verificacao}')
 
-    print(f'Termino : {file}')
-    
+        if tamanho_original == tamanho_apos_verificacao:
+            continue
+        
+        try:
+            with open(f'{path}/{file}', "w", encoding="utf-8") as f:
+                json.dump(new_data, f, ensure_ascii=False, indent=2)
+        except:
+            print(f"Erro no arquivo -> {file}")
+
+        print(f'Termino : {file}')
+
+
+if __name__ == "__main__":
+    run()
